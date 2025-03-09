fd0cc7022fcd7f67809f7fab2875e1cbc228faed
sokomin <sokomincontact@gmail.com>
2025-03-09 22:07:12 +0900

    お試しアップロード

---
+++ db/mobdb/mobdb1094.js
@@ -0,0 +1,12 @@
+MobData = {
+	1091: [ {"id":"0","inid":0,"type":1,"name":"進行ガイド","repop":120,"id_area":256,"lv_min":0,"lv_max":0,"is_npc":false,"real_posx":26.41,"real_posy":29.06,"posx":null,"posy":null},
+]};
+MobList = {
+	1091: ["モンスター", "進行ガイド",],
+};
+
+AreaData = {
+	1091: [ {"id":"0","type":0,"name":"_필드 전체","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":null,"posx2":null,"posy":null,"posy2":null},
+	{"id":"1","type":0,"name":"_화면","access_map":"","is_secret":0,"real_posx":0,"real_posx2":0,"real_posy":0,"real_posy2":0,"posx":null,"posx2":null,"posy":null,"posy2":null},
+	{"id":"2","type":5,"name":"시작 영역","access_map":"","is_secret":0,"real_posx":32.19,"real_posx2":38.69,"real_posy":25.56,"real_posy2":30.06,"posx":null,"posx2":null,"posy":null,"posy2":null},
+]};
